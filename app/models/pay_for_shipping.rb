d209b7c66065ea8390ba865bf60176e10dce8c41
kazuki0157 <apokali0157@gmail.com>
2022-07-10 13:36:39 +0900

    バリデーション追加

--- app/models/pay_for_shipping.rb
+++ app/models/pay_for_shipping.rb
@@ -1,8 +1,8 @@
 class PayForShipping < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 1, name: '着払い(購入者負担)' },
-    { id: 2, name: '送料込み(出品者負担)' }
+    { id: 1, name: '着払い希望(購入者負担)' },
+    { id: 2, name: '送料込み希望(出品者負担)' }
   ]
   include ActiveHash::Associations
   has_many :items
