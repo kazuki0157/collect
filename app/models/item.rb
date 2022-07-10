7c5342ea7811baccfcab75f4c542095bab15ab74
kazuki0157 <apokali0157@gmail.com>
2022-07-10 13:19:24 +0900

    商品出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :pay_for_shipping
+  belongs_to :ken_name
+  belongs_to :shipping_days
+
 end
