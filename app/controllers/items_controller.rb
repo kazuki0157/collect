c9dc8217a1cd73c0a5a4c65f1afc5906a3a66b4b
kazuki0157 <apokali0157@gmail.com>
2022-07-14 16:07:37 +0900

    商品編集画面作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,14 +21,23 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
-  
+  def edit
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :product_name, :description, :condition_id, :pay_for_shipping_id,
                                  :ken_name_id, :shipping_days_id).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
