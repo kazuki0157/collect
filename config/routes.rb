ab1277d02e8582382d2768b3b1134a5f7b19ece5
kazuki0157 <apokali0157@gmail.com>
2022-07-14 18:14:59 +0900

    tradeルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'trades/index'
   devise_for :users
   root to: "items#index"
-  resources :items
+  resources :items do
+    resources :trade, only: [:index, :new, :create]
+  end
 end
