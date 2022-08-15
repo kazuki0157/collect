412bb78789131acec81836b81ab1050e78158898
kazuki0157 <apokali0157@gmail.com>
2022-08-15 11:13:55 +0900

    商品トレード画面作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,14 +4,15 @@ class OrdersController < ApplicationController
     @nickname = current_user.nickname
     trade_items = Relationship.where(item_id: current_user.id)
     @my_item = current_user.items
-    #@item = Item.find(params[:item_id])
-    #@items = Item.where(id: trade_items[trade_item_id])
-    #@items = Item.includes(:relationship) # Owner.allから変更
     @items = Item.where(id: trade_items.map(&:trade_item))
   end
 
   def show
     @item = Item.find(params[:item_id])
+  end
+
+  def create
+
   end
 
 end
